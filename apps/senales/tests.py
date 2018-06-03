776104144893526ed00e2ea14094cd512d445e18
maurimorero <maurimorero@gmail.com>
2018-06-03 16:55:01 -0300

    1.0 ok

--- apps/senales/tests.py
+++ apps/senales/tests.py
@@ -11,6 +11,6 @@ class ChasisTests(TestCase):
     def test_chasis_valido(self):
         todos = Chasis.objects.all()
         c1= Chasis()
-        c1.name="C1"
+        c1.name="C10"
         c1.description="asdasds"
         self.assertEqual(c1.name, "C10")
